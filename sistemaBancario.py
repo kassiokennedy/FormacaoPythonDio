254bcb5e77e743bf12e96d799d8f1e5916f3c737
kassiokennedy <kassio1206@hotmail.com>
2022-10-14 20:00:01 -0300

    desafio

--- sistemaBancario.py
+++ sistemaBancario.py
@@ -1,53 +1,91 @@
+'''
+https://academiapme-my.sharepoint.com/:p:/g/personal/kawan_dio_me/Ef-dMEJYq9BPotZQso7LUCwBJd7gDqCC2SYlUYx0ayrGNQ?rtime=6BGAHCWu2kg
+
+'''
+# Variavel global
+saldo = 1000
+i = 0
+extrato = ''
 class Banco:
 
     def __init__(self):
         pass
-
+    # --------------------------------------------------------
     def depositar(self):
-        saldo = 0
-        deposito = float(input("Deposite seu dinheiro: "))
+        global saldo
+        global extrato
+        deposito = float(input("\nDeposite seu dinheiro: "))
         if deposito <= 0:
-            print("Nao é permitido deposito negativo ou valor zero.")
+            print("\nNao é permitido deposito negativo ou valor zero.")
         else:
             saldo += deposito
-        return print(saldo)
-
+        extrato += f'\nDepósito: {deposito:.2f} R$'
+        return print(f'Seu saldo é de {saldo:.2f} R$\n')
+    # -------------------------------------------------------
     def sacar(self):
-
-        saldo = 1000
-        saque = float(input(
-            f'Seu saldo é: {saldo}\n'
-            f'Quanto deseja sacar? '))
-        if saque <= 0:
-            print("Nao é permitido sacar valor zero.")
-            if saque > saldo:
-                print("Saldo insuficiente.")
+        global saldo # chamada da variavel global
+        global i
+        global extrato
+        if saldo > 0 and i <=4: # Verifica se ha saldo para a operacao
+            saque = float(input(
+                f'\nSeu saldo é de {saldo:.2f} R$\n'
+                f'Informe o valor do saque: '))
+            if saque <= 0 or saque > 500:
+                return print("Operação inválida!.")
+            else:
+                saldo -= saque
+            extrato += f'\nSaque: {saque:.2f} R$'
+            i += 1
         else:
-            saldo -= saque
-        return print(saldo)
+            if saldo <= 0:
+                print("\nSaldo insuficiente.")
+            else:
+                print("\nSeulimite é de ate 3 saques diários.")
 
+        return print(f'\nSeu saldo é de {saldo:.2f} R$\n')
+    # -------------------------------------------------------
     def extrato(self):
-        pass
+        if not extrato:
+            print(f'''
+-------------------- Extrato --------------------------- 
+            Não ha movimentações.
+--------------------------------------------------------
+                        ''')
+        else:
+            print(f'''
+-------------------- Extrato --------------------------- 
+{extrato}
+\nSeu saldo é de {saldo:.2f} R$\n
+--------------------------------------------------------
+            ''')
 
+#################################################################
+#################################################################
+#################################################################
 
 print(__name__)
 if __name__ == '__main__':
     # instanciando a classe Banco
     banco = Banco()
 
-    # Menu de opções
-    operacao = int(input(
-    f'''
-    Escolha uma opercação a realizar:
+    while True:
+        # Menu de opções
+        operacao = int(input(
+            f'''
+-------------------- Menu ------------------------------ 
         1 - Saque
         2 - Depósito
         3 - Extrato
-        4 - Sair
-    '''
-    ))
-    # print(f'Você digitou {operacao}')
-
-    if operacao == 1:
-        print(banco.sacar())
-    elif operacao == 2:
-        print(banco.depositar())
+        4 - Sair 
+--------------------------------------------------------
+Escolha uma operação:'''))
+        if operacao == 1:
+            print(banco.sacar())
+        elif operacao == 2:
+            print(banco.depositar())
+        elif operacao == 3:
+            print(banco.extrato())
+        elif operacao == 4:
+            print("\nFechando o programa.")
+            break
+        i += 1
