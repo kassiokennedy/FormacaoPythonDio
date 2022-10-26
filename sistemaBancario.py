ce1e032961c3041eacb8d62b7705f0fee6becc14
kassiokennedy <kassio1206@hotmail.com>
2022-10-26 20:33:03 -0300

    update no sistema bancario - criadno lista

--- sistemaBancario.py
+++ sistemaBancario.py
@@ -3,6 +3,7 @@ https://academiapme-my.sharepoint.com/:p:/g/personal/kawan_dio_me/Ef-dMEJYq9BPot
 
 https://github.com/digitalinnovationone/trilha-python-dio/blob/02_programacao_orientada_objetos/02%20-%20Programa%C3%A7%C3%A3o%20Orientada%20a%20Objetos/10%20-%20desafio/desafio_v2.py
 
+https://www.udemy.com/course/python-3-do-zero-ao-avancado/learn/lecture/15120778#overview
 
 '''
 import random
@@ -11,33 +12,67 @@ import random
 saldo = 1000
 i = 0
 extrato = ''
-class Banco:
 
+# --------------------------------------------------------
+# --------------------------------------------------------
+class Pessoa:
     def __init__(self):
         pass
-    # --------------------------------------------------------
     def novoUsuario(self):
         nome = input("Informe seu nome: ")
-        # data de nascimento
+        data_nascimento = input("Informe a data de nascimento (dd-mm-aaaa): ")
         cpf = int(input("informe somente os numeros do seu CPF: "))
         # endereço (rua, numero- bairro - cidade/estado
-        return print(f'Novo usuario criado.')
-    # --------------------------------------------------------
+        endereco = input("Informe o endereço (logradouro, num - bairro - cidade/sigla estado): ")
+
+# --------------------------------------------------------
+# --------------------------------------------------------
+class Cliente:
+    '''
+    https://github.com/kassiokennedy/cursoPython/blob/7e2536b6f0d06b4133f12adf4bc7c7b9eda16a2d/cursoudemy/Secao04/encapsulamento.py
+    '''
+    def __init__(self):
+        self.dados = {}
+
+
     def novaConta(self):
-        usuario = input("Nome de usuário: ")
+
+        nome = input("Nome de usuário: ")
+        usuario = [nome]
+
         agencia = int(1)
         conta = random.randint(0000,9999)
 
+        contas.append(novaConta)
         return print(f'''
         Usuario: {usuario}
         Agencia: {agencia}
         Conta:   {conta}
         ''')
+    def inserirCliente(self, nome,id):
+        nome = input("Nome de usuário: ")
+        id = input("Nome de usuário: ")
+        if 'clientes' not in self.dados:
+            self.dados['clientes'] = {id: nome}
+        else:
+            self.dados['clientes'].update({id: nome})
+
+    def listaClientes(self):
+        for id, nome in self.dados['clientes'].items():
+            print(id, nome)
+
+
+# --------------------------------------------------------
+# --------------------------------------------------------
+class Banco:
+
+    def __init__(self):
+        pass
 
     # --------------------------------------------------------
     def lista(self):
 
-        if banco.novaConta == None:
+        if not Cliente.novaConta:
             print("Nada aqui.")
         else:
             print("erro")
@@ -99,8 +134,10 @@ class Banco:
 
 # print(__name__)
 if __name__ == '__main__':
-    # instanciando a classe Banco
+    # instanciando as classes
     banco = Banco()
+    pessoa = Pessoa()
+    cliente = Cliente()
 
     while True:
         # Menu de opções
@@ -117,9 +154,9 @@ if __name__ == '__main__':
 --------------------------------------------------------
 Escolha uma operação:'''))
         if operacao == 1:
-            print(banco.novoUsuario())
+            print(pessoa.novoUsuario())
         elif operacao == 2:
-            print(banco.novaConta())
+            print(cliente.inserirCliente(any, any))
         elif operacao == 3:
             print(banco.depositar())
         elif operacao == 4:
@@ -127,7 +164,7 @@ Escolha uma operação:'''))
         elif operacao == 5:
             print(banco.extrato())
         elif operacao == 6:
-            print(banco.lista())
+            print(cliente.listaClientes())
         elif operacao == 7:
             print("\nFechando o programa.")
             break
