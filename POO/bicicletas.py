6393635855040f69006832fe9a1d55a1d7fea500
Kassio <kassio1206@hotmail.com>
2022-11-08 10:59:15 -0300

    atualizaçao do banco

---
+++ POO/bicicletas.py
@@ -0,0 +1,9 @@
+class Bicicleta:
+    def __int__(self, cor, modelo, ano, valor):
+        self.cor = cor
+        self.modelo = modelo
+        self.ano = ano
+        self.valor = valor
+
+
+caloi = Bicicleta()
