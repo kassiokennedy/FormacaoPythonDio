eea538de650c0da3019c46e0384c12c80dfc5da0
kassiokennedy <kassio1206@hotmail.com>
2022-11-11 18:33:23 -0300

    POO

---
+++ POO/construtor_destrutor.py
@@ -0,0 +1,37 @@
+'''
+    Métodos
+construtor ___init___
+destrutor ___del___
+'''
+
+class Cachorro:
+    def __init__(self, nome, cor, acordado=True):
+        print("Inicializando a classe...")
+        self.nome = nome
+        self.cor = cor
+        self.acordado = acordado
+
+    def __del__(self):
+        print("Removendo a instância da classe.")
+
+    def falar(self):
+        print("auau")
+
+
+def criar_cachorro():
+    c = Cachorro("Zeus", "Branco e preto", False)
+    print(c.nome)
+
+
+c = Cachorro("Chappie", "amarelo")
+c.falar()
+
+print("Ola mundo")
+
+del c
+
+print("Ola mundo")
+print("Ola mundo")
+print("Ola mundo")
+
+# criar_cachorro()
