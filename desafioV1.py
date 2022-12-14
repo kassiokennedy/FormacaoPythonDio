d9541e323aeacab6daea52d109171272e8d050fb
Kassio Vieira <kassio1206@hotmail.com>
2022-12-14 07:39:11 -0300

    desafiov1

--- desafioV1.py
+++ desafioV1.py
@@ -1,3 +1,6 @@
+from abc import ABC, abstractclassmethod, abstractclassproperty
+from datetime import datetime
+
 '''
 Desafio do sistema bancario com POO
 
@@ -5,8 +8,43 @@ Desafio do sistema bancario com POO
 
 
 class Cliente:
+    def __init__(self, endereco):
+        self.endereco = endereco
+        self.contas = []
+
+    def realizarTransacao(self, conta, transacao):
+        transacao.registrar(conta)
+
+    def addConta(self,conta):
+        self.contas.append(conta)
+
+
+class PessoaFisica:
+    def __init__(self, nome, nascimento, cpf, endereco):
+        super().__init__(endereco)
+        self.nome = nome
+
+
+class Conta:
     pass
 
 
-class Teste:
+class CC:
     pass
+
+
+class Historico:
+    pass
+
+
+class Transacao(ABC):
+    pass
+
+
+class Saque(Transacao):
+    pass
+
+
+class Deposito(Transacao):
+    pass
+
